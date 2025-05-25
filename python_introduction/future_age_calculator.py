a5a6ec058bb0be4f76275f6895def461e1bd3e85
Kevin-Wambua <766KevinWambua@gmail.com>
2025-05-25 21:00:14 +0300

    Add all required python_introduction files

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+#Assign variables for age and years to add
+age = int(30)  # Current age in years
+years_to_add = 27  # Number of years to add
+# Calculate future age
+future_age = age + years_to_add
+# Print the result
+print(f"In 2050, you will be {future_age} years old.")
+# Output: In 2050, you will be 57 years old.
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,8 @@
+#Assign values for length and width
+length = 10
+width = 5
+# Calculate area of rectangle
+area = length * width
+# Print the result
+print(f"The area of the rectangle is:", area)
+# Output: The area of the rectangle is: 50
