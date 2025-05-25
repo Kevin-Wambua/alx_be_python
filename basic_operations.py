b8a9d0298e2c216f1510ac8c0ab545605dc413ec
Kevin-Wambua <766KevinWambua@gmail.com>
2025-05-25 14:33:21 +0300

    Initial commit: add python_introduction exercises

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+# Assign values to the variables
+number1 = 10
+number2 = 5
+
+ # perform arithmetic operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+#print the results 
+print("Addition of", number1, "and", number2,"is:", sum_result)
+print("subtraction of", number1, "and", number2,"is:", difference_result)
+print("multiplication of", number1,"and", number2,"is:",product_result)
