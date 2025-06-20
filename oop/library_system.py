6a667c9388310a3721d6f2bb97c47941677a1045
Kevin-Wambua <766KevinWambua@gmail.com>
2025-06-20 09:55:41 -0700

    updated library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,31 +4,31 @@ class Book:
         self.title = title
         self.author = author
 
-    def get_info(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
-# Derived class
+# Derived class: EBook
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_info(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
-# Derived class
+# Derived class: PrintBook
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_info(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
-# Composition
+# Library class using composition
 class Library:
     def __init__(self):
         self.books = []
@@ -38,4 +38,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book.get_info())
+            print(book)  # This will call __str__()
